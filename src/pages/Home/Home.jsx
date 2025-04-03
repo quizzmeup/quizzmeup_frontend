0b3e5d7b318ccac8afa11ee53ddad1402dae45e6
Bloodymary38 <ericeric@free.fr>
2025-04-03 14:11:40 +0200

    page d'accueil, reste a integrer les composants

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,23 +3,21 @@ import "./Home.css";
 import axios from "axios";
 import { useState, useEffect } from "react";
 import { getQuizzes } from "../../api/home";
+import { useNavigate } from "react-router-dom";
+
+import { FaExternalLinkAlt } from "react-icons/fa";
 
 const Home = () => {
   const { userData } = useAuth();
-
   const [data, setData] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
 
   // Fonction pour récupérer les quiz
   const fetchData = async () => {
-    //console.log("appel a fetchdata");
-
     setIsLoading(true);
     try {
       const response = await getQuizzes();
       //const response = await axios.get("http://localhost:3000/api/quizzes");
-
-      //      console.log("retour api ok");
 
       setData(response.data);
 
@@ -34,7 +32,11 @@ const Home = () => {
     fetchData();
   }, []);
 
-  const handleClick = () => {};
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate("/backoffice/backoffice");
+  };
 
   return !userData ? (
     <main className="container">
@@ -47,6 +49,9 @@ const Home = () => {
             <div className="line_formulaire" key={index}>
               <h2>{elem.title}</h2>
 
+              <div className="line_formulaire" onClick={handleClick}>
+                <FaExternalLinkAlt size={30} color="white" />
+              </div>
               <button className="auth-btn" onClick={handleClick}>
                 Clique moi
               </button>
